79029a2da74988e413d06a3f9e77c1860197e268
Vincent Duchauffour <40724343+VDuchauffour@users.noreply.github.com>
2026-06-02 11:10:37 +0200

    chore: refacto main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use anyhow::Result;
-
 pub mod cli;
 pub mod helper;
 pub mod http;
@@ -9,19 +7,3 @@ pub mod providers;
 pub mod render;
 pub mod report;
 pub mod style;
-
-use login::cmd_login;
-use report::cmd_report;
-
-pub fn run(cli: cli::Cli) -> Result<()> {
-    match cli.command {
-        cli::Command::Login {
-            workspace_id,
-            server_id,
-            function_id,
-            sub_day,
-        } => cmd_login(workspace_id, server_id, function_id, sub_day),
-
-        cli::Command::Report => cmd_report(),
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use anyhow::Result;
 use clap::Parser;
-pub mod style;
-use style::{RED, RESET};
 
+use usus::style::{RED, RESET};
+use usus::{cli, login, report};
+
+pub fn run(cli: cli::Cli) -> Result<()> {
+    match cli.command {
+        cli::Command::Login {
+            workspace_id,
+            server_id,
+            function_id,
+            sub_day,
+        } => login::cmd_login(workspace_id, server_id, function_id, sub_day),
+
+        cli::Command::Report => report::cmd_report(),
+    }
+}
 fn main() {
-    let cli = usus::cli::Cli::parse();
-    if let Err(e) = usus::run(cli) {
+    let cli = cli::Cli::parse();
+    if let Err(e) = run(cli) {
         eprintln!("{RED}Error:{RESET} {e:#}");
         std::process::exit(1);
     }
